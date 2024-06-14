9fa222217f61ac38b3cd67fa9fc033bb72644aa6
Le Chen <LeChen11111@163.com>
2024-06-14 22:21:11 +0800

    update

--- event_loop/setTimeout.c
+++ event_loop/setTimeout.c
@@ -16,31 +16,29 @@ void my_func(void *arg) {
     printf("my_func arg is:%d\n", *my_func_arg);
 }
 
-void setTimeout(func_ptr fun, int time, void *arg) {
-    sleep(time); //以秒为单位
-    (*fun)(arg);
-}
-
 void *thread_func(void *args) {
     TimeoutArgs timeoutArgs = (TimeoutArgs)args;
-    setTimeout(timeoutArgs->fun, timeoutArgs->time, timeoutArgs->arg);
+    timeoutArgs->fun(timeoutArgs->arg);
     free(timeoutArgs);
     return NULL;
 }
 
-int main() {
-    int arg = 1;
-    pthread_t thread1;
-
+void setTimeout(func_ptr fun, int time, void *arg) {
     TimeoutArgs args = (TimeoutArgs)malloc(sizeof(struct timeout_args));
     args->fun = my_func;
     args->time = 1;
-    args->arg = &arg;
+    args->arg = arg;
 
-    pthread_create(&thread1, NULL , thread_func, args);
-    printf("OK\n");
-    pthread_join(thread1, NULL);
+    pthread_t thread;
     
+    pthread_create(&thread, NULL, thread_func, args);
+    sleep(time); //以秒为单位
+    pthread_join(thread, NULL);
+}
+
+int main() {
+    int arg = 1;
+    setTimeout(my_func, 1, &arg);
 
     return 0;
 }
