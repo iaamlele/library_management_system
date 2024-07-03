03b40ac46fdc2dd39121a7558073eab568f19593
Le Chen <LeChen11111@163.com>
2024-07-03 10:28:58 +0800

    update

--- learning-js/jquery.js
+++ learning-js/jquery.js
@@ -3,15 +3,26 @@
 // 2.实现每一类功能函数
     // jQuery中所有选择器都以美元符号开头：$()
 
-console.log("hello js");
+// console.log("hello js");
 
-//通过id查找html元素
-let x = document.getElementById("first");
-console.log(x);
-//通过标签名查找html元素
-let y = x.getElementsByTagName("p");
+// //通过id查找html元素
+// let x = document.getElementById("first");
+// console.log(x);
+// //通过标签名查找html元素
+// let y = x.getElementsByTagName("p");
 
-let a = document.getElementById("second");
-//通过类名查找html元素
-let b = a.getElementsByClassName("second-class1");
-let c = b.getElementsByTagName("p");
+// let a = document.getElementById("second");
+// //通过类名查找html元素
+// let b = a.getElementsByClassName("second-class1");
+// let c = b.getElementsByTagName("p");
+
+
+const jsdom = require("jsdom");
+const {JSDOM} = jsdom;
+const fs = require("fs");
+
+const html = fs.readFileSync("jquery.html", "utf-8");
+
+const dom = new JSDOM(html);
+const document = dom.window.document;
+console.log(document.querySelector("p").textContent);
