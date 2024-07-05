4b5a8c5ab14107b6ddc2c7266363e564ac03c2d2
Le Chen <LeChen11111@163.com>
2024-07-05 21:01:20 +0800

    update

--- learning-js/jquery.js
+++ learning-js/jquery.js
@@ -1,16 +1,20 @@
 // 1.js怎么与网页连接->把js放在html中
 // 2.js怎么获取网页中的元素->js HTML DOM->jsdom
 // 2.实现每一类功能函数
-    // jQuery中所有选择器都以美元符号开头：$()
-module.exports = function() {
-    click_hide;
-    click_hide_addListener;
-}
+module.exports.click_hide = click_hide;
+module.exports.click_hide_addListener = click_hide_addListener;
+
+const jsdom = require("jsdom");
+const {JSDOM} = jsdom;
+const fs = require("fs");
+const html = fs.readFileSync("index.html", "utf-8");
+const dom = new JSDOM(html, {resources: "usable", runScripts: "dangerously"});
+const document = dom.window.document;
 
 function click_hide(element) {
     const hide_element = document.querySelector(element)
-    hide_element.hidden = true;
-    console.log(p.hidden);
+    hide_element.style.visibility = 'hidden';
+    console.log(hide_element.style.visibility);
 }
 
 function click_hide_addListener(elem1, elem2) {
