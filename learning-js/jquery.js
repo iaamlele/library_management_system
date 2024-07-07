8f6f53ac0c9c95bbc09735bd9428709fbd3dd835
Le Chen <LeChen11111@163.com>
2024-07-07 11:32:57 +0800

    update

--- learning-js/jquery.js
+++ learning-js/jquery.js
@@ -1,25 +1,17 @@
 // 1.js怎么与网页连接->把js放在html中
-// 2.js怎么获取网页中的元素->js HTML DOM->jsdom
-// 2.实现每一类功能函数
-module.exports.click_hide = click_hide;
-module.exports.click_hide_addListener = click_hide_addListener;
-
-const jsdom = require("jsdom");
-const {JSDOM} = jsdom;
-const fs = require("fs");
-const html = fs.readFileSync("index.html", "utf-8");
-const dom = new JSDOM(html, {resources: "usable", runScripts: "dangerously"});
-const document = dom.window.document;
-
+// 2.js怎么获取网页中的元素->js HTML DOM->jsdom  这样不方便->浏览器
+// 3.实现显示和隐藏功能
+module.exports
+//隐藏和显示功能
 function click_hide(element) {
-    const hide_element = document.querySelector(element)
+    const hide_element = document.querySelector(element);
     hide_element.style.visibility = 'hidden';
     console.log(hide_element.style.visibility);
 }
 
 function click_hide_addListener(elem1, elem2) {
     const element1 = document.querySelector(elem1);
-    element1.addEventListener("click", click_hide(elem2));
+    element1.addEventListener("click", function() {
+        click_hide(elem2);
+    });
 }
-
-
--- learning-js/test.js
+++ learning-js/test.js
@@ -1,20 +1,9 @@
-const jsdom = require("jsdom");
-const {JSDOM} = jsdom;
-const simulate_jquery = require("./jquery.js");
-const fs = require("fs");
-
-const html = fs.readFileSync("index.html", "utf-8");
-// resources: 'usable'代表js脚本在外部  
-// runScripts:'dangerously'不添加会导致选项控制不起作用
-const dom = new JSDOM(html, {resources: "usable", runScripts: "dangerously"});
-
 // 确保所有资源加载完成后执行脚本
 dom.window.onload = () => {
     const document = dom.window.document;
-    simulate_jquery.click_hide_addListener("#button1", "#first-id");
-    
     const button = document.getElementById("button1");
+    simulate_jquery.click_hide_addListener(document, "#button1", "#first-id");
     button.click();
-    
-    console.log(document.getElementById("first-id").style.visibility);
+
+    console.log(document.getElementById("first-id").style.visibility); //不知道为什么这里显示不出来
 };
